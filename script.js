277a646b23dc0de220b942f2b8c995a04c28c95f
Cadu Dantas <cadudantasgja@icloud.com>
2026-08-27 17:13:58 -0300

    arrumando o site + usando o java

--- script.js
+++ script.js
@@ -1,4 +1,80 @@
+anispeed.style.display = "none";
+animagia.style.display = "none";
+anitank.style.display = "none";
+anibrutos.style.display = "none";
+statsaniBT.style.display = "none";
+textaniBT.style.display = "none";
+placaaniBT.style.display = "none";
+tituloaniBT.style.display = "none";
+psdescricaoaniBT.style.display = "none";
+descricaoaniBT.style.display = "none";
+pshistoriaaniBT.style.display = "none";
+historiaaniBT.style.display = "none";
+botaonexosvoltar.style.display = "none";
+
+
 let botaopi = document.getElementById("pi");
 botaopi.onclick = function() {
    window.location.href = "index.html"
     };
+
+let nexosbotao = document.getElementById("botaonexosmostrar");
+
+nexosbotao.onclick = function() {
+
+    nexus.style.display = "none";
+    statsnexos.style.display = "none";
+    textnexos.style.display = "none";
+    textnexos2.style.display = "none";
+    placanexos.style.display = "none";
+    titulonexos.style.display = "none";
+    psdescricaonexos.style.display = "none";
+    descricaonexos.style.display = "none";
+    pshistorianexos.style.display = "none";
+    historianexos.style.display = "none";
+    anibrutos.style.display = "block"
+    statsaniBT.style.display = "block";
+    textaniBT.style.display = "block";
+    placaaniBT.style.display = "block";
+    tituloaniBT.style.display = "block";
+    psdescricaoaniBT.style.display = "block";
+    descricaoaniBT.style.display = "block";
+    pshistoriaaniBT.style.display = "block";
+    historiaaniBT.style.display = "block";
+    botaonexosmostrar.style.display = "none";
+    botaonexosvoltar.style.display = "block";
+
+
+    
+
+};
+
+let nexosbotao2 = document.getElementById("botaonexosvoltar");
+
+nexosbotao2.onclick = function() {
+
+    nexus.style.display = "block";
+    statsnexos.style.display = "block";
+    textnexos.style.display = "block";
+    textnexos2.style.display = "block";
+    placanexos.style.display = "block";
+    titulonexos.style.display = "block";
+    psdescricaonexos.style.display = "block";
+    descricaonexos.style.display = "block";
+    pshistorianexos.style.display = "block";
+    historianexos.style.display = "block";
+    anibrutos.style.display = "none"
+    statsaniBT.style.display = "none";
+    textaniBT.style.display = "none";
+    placaaniBT.style.display = "none";
+    tituloaniBT.style.display = "none";
+    psdescricaoaniBT.style.display = "none";
+    descricaoaniBT.style.display = "none";
+    pshistoriaaniBT.style.display = "none";
+    historiaaniBT.style.display = "none";
+    botaonexosmostrar.style.display = "block";
+    botaonexosvoltar.style.display = "none";
+
+    
+
+};
