2c70ede27ad83da92e1f2b1ec9bcaa790577516f
Cadu Dantas <cadudantasgja@icloud.com>
2026-09-08 16:45:56 -0300

    Mega att do site, finalizado primeira parte

---
+++ Primeira parte NOVO/scriptpaginainicial.js
@@ -0,0 +1,64 @@
+annistates.style.display = "none"
+
+let titulodiferenciado = document.getElementById("titulodiferenciado")
+
+    titulodiferenciado.onclick = function () {
+
+     celestiais.style.display = "none";
+
+     annistates.style.display = "block";
+
+    }
+
+let titulodiferenciado1 = document.getElementById("titulodiferenciado1")
+
+    titulodiferenciado1.onclick = function () {
+
+     celestiais.style.display = "block";
+
+     annistates.style.display = "none";
+
+    }
+
+let  paginainicial = document.getElementById("paginainicial")
+
+   paginainicial.onclick = function() {
+
+        window.location.href = "paginainicial.html";
+
+    }    
+
+let  historia = document.getElementById("historia")
+
+    historia.onclick = function() {
+
+        window.location.href = "historia.html";
+
+    }
+
+let  index = document.getElementById("index")
+
+    index.onclick = function() {
+
+        window.location.href = "index.html";
+
+    }
+
+let  OJ = document.getElementById("OJ")
+
+    OJ.onclick = function() {
+
+        window.location.href = "ot.html";
+
+    }
+
+let  creditos = document.getElementById("creditos")
+
+    creditos.onclick = function() {
+        
+        window.location.href = "creditos.html";
+
+    }
+
+
+
