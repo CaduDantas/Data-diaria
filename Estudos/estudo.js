f8d1628ab4c7ee4a77cd81d7f4430c2fd838ca66
Cadu Dantas <cadudantasgja@icloud.com>
2026-09-13 22:37:08 -0300

    arrumando algumas coisas

---
+++ Estudos/estudo.js
@@ -0,0 +1,9 @@
+
+var nome = prompt ('escreva seu nomezin')
+document.write (`<p class="titulo"> o seu nome e ${nome} </p>`)
+document.write (`<p class="titulo"> Seu nome tem ${nome.length} </p>`)
+document.write (`<p class="titulo"> Seu nome em maiusculo e  ${nome.toUpperCase()} </p>`)
+document.write (`<p class="titulo"> Seu nome em minusculo e  ${nome.toLowerCase()} </p>`)
+
+
+
